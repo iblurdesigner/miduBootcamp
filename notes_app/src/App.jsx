dac3710eb11ac72904413b322c85d7f2cb9d42d2
iblurdesigner <nevomen@gmail.com>
2022-12-22 16:57:59 -0500

    parte 2 del curso terminada Fetching de datos

--- notes_app/src/App.jsx
+++ notes_app/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import { Note } from './components/Note';
+import {create as createNote, getAll as getAllNotes} from './services/notes'
 
 export default function App(props) {
   // if (typeof notes === 'undefined' && notes.length === 0) {
@@ -10,12 +11,32 @@ export default function App(props) {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   // const [showAll, setShowAll] = useState(true);
+  const [loading, setLoading] = useState(false)
 
+  // *****  useEffect con FETCH
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((response) => response.json())
+  //     .then((json) => setNotes(json));
+  // }, []);
+
+
+  // *****  useEffect con AXIOS
   useEffect(() => {
     console.log('useEffect');
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((json) => setNotes(json));
+    setLoading(true)
+    getAllNotes()
+    .then(notes => {
+      setNotes(notes)
+      setLoading(false)
+    })
+    // axios.get('https://jsonplaceholder.typicode.com/posts')   YA NO VAMOS A USAR esto si no lo separamos la logica. En services
+    //   .then(response => {
+    //     const {data} = response
+    //     setNotes(data)
+    //     setLoading(false)
+    //   });
   }, []);
 
   const handleChange = (event) => {
@@ -25,24 +46,42 @@ export default function App(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const noteToAddToState = {
-      id: notes.length + 1,
+      // id: notes.length + 1,
       // content: newNote,
       title: newNote,
       body: newNote,
+      userId: 1,
       // date: new Date().toISOString(),
       // important: Math.random() < 0.5,
     };
 
     console.log(noteToAddToState);
 
-    // setNotes(notes.concat(noteToAddToState));
-    setNotes([...notes, noteToAddToState]);
+    createNote(noteToAddToState)
+      .then( newNote => {
+        setNotes(prevNotes =>  prevNotes.concat(newNote))
+      })
+      .catch( e => {
+        console.error(e)
+      })
+
+    // ***** PARA GUARDAR UNA NOTA        YA NO VAMOS A USAR esto si no lo separamos la logica. En services
+    // axios.post('https://jsonplaceholder.typicode.com/posts', noteToAddToState)
+    //   .then(response => {
+    //     const {data} = response
+    //     setNotes(prevNotes =>  prevNotes.concat(data))
+    //   })
+
+    // // setNotes(notes.concat(noteToAddToState));
+    // // setNotes([...notes, noteToAddToState]);
+    
     setNewNote('');
   };
 
   // const handleShowAll = () => {
   //   setShowAll(() => !showAll);
   // };
+  { setLoading ? "Cargando" : ""}
 
   return (
     <div>
